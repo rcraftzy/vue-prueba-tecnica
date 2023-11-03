69a5eb2ba28edbed9542cc6c1b7e5bd005aea1e7
roberto-carlos-tg <roberto.tg99@gmail.com>
2023-11-03 08:56:47 -0500

    :sparkles: Add primeicons import

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import 'primevue/resources/themes/lara-light-teal/theme.css'
+import 'primeicons/primeicons.css'
 import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config'
