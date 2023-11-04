6672e91b7a2fb0a84d6ea3e8733677a4d95f94db
roberto-carlos-tg <roberto.tg99@gmail.com>
2023-11-03 21:31:08 -0500

    :wrench: Removed the router configuration

--- src/main.js
+++ src/main.js
@@ -2,12 +2,10 @@ import { createApp } from 'vue'
 import 'primevue/resources/themes/lara-light-teal/theme.css'
 import 'primeicons/primeicons.css'
 import App from './App.vue'
-import router from './router'
 import PrimeVue from 'primevue/config'
 import store from './store'
 
 createApp(App)
   .use(store)
   .use(PrimeVue)
-  .use(router)
   .mount('#app')
