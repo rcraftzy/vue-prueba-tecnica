86d2aa73ec15761b89b9f31b56e26faa3355bae2
roberto-carlos-tg <roberto.tg99@gmail.com>
2023-11-06 17:18:19 -0500

    :sparkles: Feat toastservice to app vue

--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ import 'primeicons/primeicons.css'
 import App from './App.vue'
 import PrimeVue from 'primevue/config'
 import store from './store'
+import ToastService from 'primevue/toastservice'
 
 createApp(App)
   .use(store)
   .use(PrimeVue)
+  .use(ToastService)
   .mount('#app')
+document.title = 'Prueba tecnica Vue3 | Berht'
